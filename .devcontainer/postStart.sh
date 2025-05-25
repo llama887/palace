e67ab83656cbbde97518d5ad9bee5d46b015ce29
llama887 <mail.fyiu@gmail.com>
2025-05-25 02:10:18 +0000

    initial commit with working react docker container

---
+++ .devcontainer/postStart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if GitHub CLI is already authenticated
+if gh auth status >/dev/null 2>&1; then
+  echo "You are already logged in to GitHub CLI. Skipping authentication."
+else
+  echo "Logging in to GitHub CLI..."
+  gh auth login
+fi
+
+gh auth status
+
+# Set the Git remote URL
+git remote set-url origin https://github.com/llama887/palace.git 
